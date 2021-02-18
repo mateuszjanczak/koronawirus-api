e19d8cf1487e2e667d6298af0f010833fa642c6b
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-02-18 16:38:25 +0100

    Fix for daily report

--- src/main/java/com/mateuszjanczak/koronawirus/api/ministerstwozdrowia/covid/general/CovidGeneralAPI.java
+++ src/main/java/com/mateuszjanczak/koronawirus/api/ministerstwozdrowia/covid/general/CovidGeneralAPI.java
@@ -6,7 +6,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface CovidGeneralAPI {
-    @GET("query?f=json&outFields=*&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2748%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false")
+    @GET("query?f=json&outFields=*&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false")
     Call<Root<CGAttributes>> getDailyReport();
 
     @GET("query?f=json&outFields=*&returnGeometry=false")
